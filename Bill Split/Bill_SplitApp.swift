e5ea1b42781996492313e2c27e98bdc3b763413d
Griffin Davis <griffin04901@gmail.com>
2025-11-21 11:32:24 -0600

    Initial Commit

---
+++ Bill Split/Bill_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bill_SplitApp.swift
+//  Bill Split
+//
+//  Created by Griffin Davis on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Bill_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bill SplitTests/Bill_SplitTests.swift
@@ -0,0 +1,17 @@
+//
+//  Bill_SplitTests.swift
+//  Bill SplitTests
+//
+//  Created by Griffin Davis on 11/21/25.
+//
+
+import Testing
+@testable import Bill_Split
+
+struct Bill_SplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
